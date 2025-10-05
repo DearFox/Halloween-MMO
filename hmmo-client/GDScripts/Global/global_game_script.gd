b64ce9517cd6620b271ef92fe89b23e53bbfee73
DearFox <DearFoxs@gmail.com>
2025-10-05 12:51:23 +0300

    Добавлена проверка статуса подключения, добавлена обработка сигнала ошибки подключения

--- hmmo-client/GDScripts/Global/global_game_script.gd
+++ hmmo-client/GDScripts/Global/global_game_script.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var ws_peer: WebSocketMultiplayerPeer
+var ws_peer_conect: bool = false
 
 # создать peer как клиент
 func create_client(url: String = "ws://localhost:1337") -> void:
@@ -8,6 +9,9 @@ func create_client(url: String = "ws://localhost:1337") -> void:
 	ws_peer = WebSocketMultiplayerPeer.new()
 	ws_peer.connect("peer_connected", Callable(self, "_on_peer_connected"))
 	ws_peer.connect("peer_disconnected", Callable(self, "_on_peer_disconnected"))
+	multiplayer.connect("connection_failed", Callable(self, "_on_connection_failed"))
+	# Сколько секунд должно пройти после потери соединения, прежде чем соединение будет разорвано со стороны клиента
+	ws_peer.set_handshake_timeout(1.0)
 	var err: Error = ws_peer.create_client(url)
 	if err != OK:
 		printerr(error_string(err))
@@ -23,6 +27,10 @@ func _on_peer_connected(id: int) -> void:
 # функция при отключении пира
 func _on_peer_disconnected(id: int) -> void:
 	print("peer_disconnected: ", id)
+
+# функция при ошибки подключения
+func _on_connection_failed() -> void:
+	print("connection_failed: Ошибка подключения!!!")
 
 # Тестовый RPC вызов
 @rpc("any_peer")
--- hmmo-client/GDScripts/mine_menu.gd
+++ hmmo-client/GDScripts/mine_menu.gd
@@ -1,9 +1,26 @@
 extends Control
 
+@onready var connection_status: Label = $VBoxMenu/ConnectionStatus
+
+var ws_peer:WebSocketMultiplayerPeer
 
 func _on_join_pressed() -> void:
 	GGS.create_client($VBoxMenu/HBoxServer/IP.text)
+	ws_peer = GGS.ws_peer
+	$ConnectionStatusCheck.start()
 
 
 func _on_button_pressed() -> void:
 	GGS.rpc_test()
+
+func _tested():
+	print("Проверка статуса")
+	if ws_peer == null:
+		connection_status.text = "Статус соединения: Не определено"
+		return
+	match ws_peer.get_connection_status():
+		0: 
+			connection_status.text = "Статус соединения: Отключен"
+			$ConnectionStatusCheck.stop()
+		1: connection_status.text = "Статус соединения: Подключение..."
+		2: connection_status.text = "Статус соединения: Подключено!"
