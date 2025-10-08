ee23c57d6869a9eb066ba721f122a992dfe9307b
DearFox <DearFoxs@gmail.com>
2025-10-08 23:08:01 +0300

    Добавлен цвет для текста состояния подключения в главном меню, удалена кнопка "тест rpc"

--- hmmo-client/GDScripts/mine_menu.gd
+++ hmmo-client/GDScripts/mine_menu.gd
@@ -10,19 +10,20 @@ func _on_join_pressed() -> void:
 	GGS.create_client($VBoxMenu/HBoxServer/IP.text)
 	ws_peer = GGS.ws_peer
 	$ConnectionStatusCheck.start()
-	
-
-
-func _on_button_pressed() -> void:
-	GGS.rpc_test()
 
 func _tested():
 	if ws_peer == null:
 		connection_status.text = "Статус соединения: Не определено"
+		connection_status.modulate = Color(1.0, 1.0, 1.0, 1.0)
 		return
 	match ws_peer.get_connection_status():
 		0: 
 			connection_status.text = "Статус соединения: Отключен"
+			connection_status.modulate = Color(1.0, 0.0, 0.0, 1.0)
 			$ConnectionStatusCheck.stop()
-		1: connection_status.text = "Статус соединения: Подключение..."
-		2: connection_status.text = "Статус соединения: Подключено!"
+		1: 
+			connection_status.text = "Статус соединения: Подключение..."
+			connection_status.modulate = Color(0.0, 1.0, 1.0, 1.0)
+		2: 
+			connection_status.text = "Статус соединения: Подключено!"
+			connection_status.modulate = Color(0.0, 1.0, 0.0, 1.0)
