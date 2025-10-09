23551132bd72dff0a7f2fe937a32706e3d40cdfd
DearFox <DearFoxs@gmail.com>
2025-10-09 23:14:38 +0300

    прыжок

--- hmmo-client/GDScripts/player.gd
+++ hmmo-client/GDScripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-#const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 4.5
 
 var player_current: bool = false
 var player_color:Color = Color(0.0, 0.29, 3.413)
@@ -26,7 +26,9 @@ func _physics_process(delta: float) -> void:
 		# Add the gravity.
 		if not is_on_floor():
 			velocity += get_gravity() * delta
-
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
