4133fa7734800e9bf98ecc37f61eca0f195cb3a6
DearFox <DearFoxs@gmail.com>
2026-08-24 23:45:41 +0300

    Управление с геймпада, нахождение локального сервера. Текстовый ввод с геймпада пока не реализован.

--- hmmo-client/GDScripts/mine_menu.gd
+++ hmmo-client/GDScripts/mine_menu.gd
@@ -6,6 +6,19 @@ var ws_peer:WebSocketMultiplayerPeer
 
 func _ready() -> void:
 	$VBoxMenu/HBoxPlayerName/PlayerName.text = pdb.PlayerName
+	ServiceDiscovery.port = 4040
+	ServiceDiscovery.scanned_server.connect(_on_scan_server)
+	ServiceDiscovery.scanned.connect(_on_scan_scanned)
+	$VBoxMenu/HBoxServer/OptionButton.grab_focus()
+
+func _on_scan_server(data):
+	add_server_button(data)
+
+func add_server_button(data):
+	$VBoxMenu/HBoxServer/OptionButton.set_item_text(2, "ws://"+str(data.server_ip)+":1337")
+	$VBoxMenu/HBoxServer/IP.text = "ws://"+str(data.server_ip)+":1337"
+
+func _on_scan_scanned():	pass
 
 func _on_join_pressed() -> void: 
 	#print($VBoxMenu/HBoxServer/OptionButton.get_item_text($VBoxMenu/HBoxServer/OptionButton.selected))
@@ -50,6 +63,8 @@ func _on_sfx_volume_slider_drag_ended(value_changed: bool) -> void:
 
 
 func _on_create_server_pressed() -> void:
+	ServiceDiscovery.server_data = {'Name':'HMMO Server'}
+	ServiceDiscovery.set_server()
 	#GGS.create_server()
 	if $VBoxMenu/HBoxPlayerName/PlayerName.text: #TODO Не позволять игрокам использовать "пустые" ники лучше.
 		pdb.PlayerName = $VBoxMenu/HBoxPlayerName/PlayerName.text
@@ -59,3 +74,7 @@ func _on_create_server_pressed() -> void:
 		GGS.create_server()
 	ws_peer = GGS.ws_peer
 	$ConnectionStatusCheck.start()
+
+
+func _on_lan_scan_timeout() -> void:
+	ServiceDiscovery.scan_lan_servers()
